9566c67f6f97cfeb061ce2cfddf2b64e4785c426
unknown <mgeun97@naver.com>
2024-10-09 11:57:44 +0900

    app.js에 라우터 추가

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import router from "../src/router.js"
 
 const app = express();
 const PORT = 3020;
 
 app.use(cookieParser());
-app.use('/')
+app.use('/', router)
 
 app.listen(PORT, () => {
     console.log(PORT, "포트로 서버가 열렸어요!");
