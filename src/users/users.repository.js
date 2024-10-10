ece96bef72f2b13ccbb109d84e25e16ae96ab11a
unknown <mgeun97@naver.com>
2024-10-10 15:54:55 +0900

    repository의 유효성 검사를 service로 이전

--- src/users/users.repository.js
+++ src/users/users.repository.js
@@ -3,31 +3,25 @@ import bcrypt from "bcrypt"
 
 export class UsersRepository {
     signUp = async (email, name, password, role) => {
-        const identifiedEmail = await prisma.users.findFirst({
-            where : {
-                email
-            }
+        await prisma.users.create({
+            data : {
+                email,
+                userName : name,
+                password,
+                role 
+            } 
         })
-        if (identifiedEmail) {
-            throw new Error ("중복된 이메일입니다.")
-        } else {
-            await prisma.users.create({
-                data : {
-                    email,
-                    userName : name,
-                    password,
-                    role 
-                } 
-            })
-        }
-            
     }
 
-    findByUser = async(email, password) => {
+    findByUser = async(email) => {
         const foundUser = await prisma.users.findUnique({ where : { email } })
-        if (!foundUser || !await bcrypt.compare(password, foundUser.password))
-            throw new Error("이메일이 존재하지 않거나 비밀번호가 일치하지 않습니다.")
         return foundUser
+    }
+
+    comparePassword = async(email, password) => {
+        const user = await prisma.users.findUnique({ where : { email } })
+        const comparePassword = await bcrypt.compare(password, user.password)
+        return comparePassword
     }
 
     profile = async(userId) => {
--- src/users/users.service.js
+++ src/users/users.service.js
@@ -20,14 +20,26 @@ export class UsersService {
             throw new Error("user와 producer 중 하나를 입력해주세요.")
         if (password !== passwordConfirm)
             throw new Error("비밀번호가 맞지 않습니다.")
+
+        const overlappingEmail = await this.usersRepository.findByUser(email)
+        if (overlappingEmail)
+            throw new Error('중복된 이메일입니다.')
+
         const hashedPassword = await bcrypt.hash(password, 10)
         await this.usersRepository.signUp(email, name, hashedPassword, role)
     }
 
     signIn = async(email, password) => {
-        if (!email || !password) throw new Error("이메일 혹은 비밀번호를 입력해주세요")
+        if (!email || !password)
+            throw new Error("이메일 혹은 비밀번호를 입력해주세요")
 
-        const user = await this.usersRepository.findByUser(email, password)
+        const user = await this.usersRepository.findByUser(email)
+        if (!user)
+            throw new Error('등록되지 않은 이메일입니다.')
+        const comparePassword = await this.usersRepository.comparePassword(email, password)
+        if (comparePassword === false)
+            throw new Error('비밀번호가 일치하지 않습니다.')
+        
         const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {expiresIn: "12h"});
         return token
     }
